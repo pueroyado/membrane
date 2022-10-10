7eab7334076e8a1377291ac36a98dc4bd724e9b4
Vadim Kravcov <v.kravtsov@yarus.ru>
2022-10-10 21:59:02 +0300

    add app files

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"demo/server"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+func main() {
+	serverApi := server.Create()
+	wg := sync.WaitGroup{}
+	ctx := context.Background()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		err := serverApi.Start()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
+
+	err := serverApi.Shutdown(ctx)
+	if err == nil {
+		log.Println("Shutdown: halted active connections", err)
+	}
+
+	wg.Wait()
+}
---
+++ server/rest.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	router     *mux.Router
+	serverHttp *http.Server
+}
+
+func Create() *APIServer {
+	return &APIServer{
+		router: mux.NewRouter(),
+	}
+}
+
+func (s *APIServer) Start() error {
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: s.RouterConf(),
+	}
+	s.serverHttp = server
+
+	return server.ListenAndServe()
+}
+
+func (s *APIServer) Shutdown(mainCtx context.Context) error {
+	ctx, cancel := context.WithTimeout(mainCtx, time.Duration(3)*time.Second)
+	defer cancel()
+
+	err := s.serverHttp.Shutdown(ctx)
+	return err
+}
+
+func (s *APIServer) RouterConf() http.Handler {
+	r := mux.NewRouter()
+	r.HandleFunc("/", s.handleInfo()).Methods(http.MethodGet)
+
+	return r
+}
+
+func (s *APIServer) handleInfo() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(w, "handleInfo")
+	}
+}
