585a62e23b0f8d69d8d5b99a455aec0bd95fe536
Vadim Kravcov <v.kravtsov@yarus.ru>
2023-01-07 21:36:07 +0300

    fix cors

--- app/server/rest.go
+++ app/server/rest.go
@@ -65,8 +65,6 @@ func (s *APIServer) Router() http.Handler {
 	secure.HandleFunc("/product", handlerProduct.List()).Methods(http.MethodGet)
 	secure.HandleFunc("/product/{id:[0-9]+}", handlerProduct.Detail()).Methods(http.MethodGet)
 
-	r.Headers("Access-Control-Allow-Origin: *")
-	r.Headers("Access-Control-Allow-Headers: Content-Type")
 	return r
 }
 
